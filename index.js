dc5e39209ef597c1ad92220f1078e31bf751ef98
Munteanu <munteanuvictoria1@gmail.com>
2022-12-02 17:04:47 +0100

    update

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ function updateTime(){
 let pragueElement = document.querySelector("#prague"); 
 let pragueDateElement=pragueElement.querySelector(".date");
 let pragueTimeElement=pragueElement.querySelector(".time");
-let pragueTime= moment().tz("Europe\Prague");
+let pragueTime= moment().tz("Europe/Prague");
 
 pragueTimeElement.innerHTML= pragueTime.format("hh:mm:ss a");
 pragueDateElement.innerHTML= pragueTime.format("ddd Do YYYY ");
-
-
 
 
 //Tokyo
@@ -24,18 +22,36 @@ tokyoTimeElement.innerHTML= tokyoTime.format("hh:mm:ss a");
 tokyoDateElement.innerHTML= tokyoTime.format("ddd Do YYYY ");
 
 
-
-
-
-//Kiev 
-let kievElement = document.querySelector("#kiev"); 
-let kievDateElement=kievElement.querySelector(".date");
-let kievTimeElement=kievElement.querySelector(".time");
-let kievTime= moment().tz("Europe/Kiev");
-
-kievTimeElement.innerHTML= kievTime.format("hh:mm:ss a");
-kievDateElement.innerHTML= kievTime.format("ddd Do YYYY ");
-
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+
+
+function updateCity(event){
+    let timeZone= event.target.value;
+    let cityTime= moment.tz(timeZone);
+    console.log(timeZone);
+    console.log(cityTime.format("MMMM Do YYYY"));
+    let allCitiesElement =document.querySelector("#cities");
+    console.log(allCitiesElement);
+    allCitiesElement.innerHTML=timeZone;
+    allCitiesElement=`
+
+    <div class="row mb-3">
+    <div class="col-6">
+      <h2>${timeZone}</h2>
+      <div class="date">${cityTime.format("ddd Do YYYY ")}</div>
+    </div>
+    <div class="col-6">
+      <div class="time">${cityTime.format("hh:mm:ss a")}></div>
+    </div>
+  </div>
+
+
+    `;
+
+}
+
+let citiesElement= document.querySelector("#city");
+citiesElement.addEventListener("change", updateCity)
